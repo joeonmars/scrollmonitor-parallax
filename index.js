d5632c8f360b0eb0bcc44694c0cc2c8b12665e4f
joeonmars <joeonmars@gmail.com>
2017-04-26 14:15:39 -0400

    update;

--- index.js
+++ index.js
@@ -8,14 +8,24 @@
 	}
 })(function (scrollMonitor) {
 
-	function SpeedParallax (element, speed) {
+	function SpeedParallax (element, speed, fade, easing) {
 		this.speed = speed;
+		this.fade = fade;
+		this.easing = easing;
 		this.element = element;
-	}
+	};
 
-	SpeedParallax.prototype.handleScroll = function (ratio, distance) {
+	SpeedParallax.prototype.handleScroll = function (distance, height) {
 		var pixels = distance * this.speed;
 		this.element.style.transform = 'translateY(' + pixels + 'px)';
+
+		if (this.fade) {
+			var ratio = distance/height;
+			ratio = Math.max(0, Math.min(1, ratio));
+			ratio = 1 - (this.easing ? this.easing(ratio) : ratio);
+
+			this.element.style.opacity = ratio;
+		}
 	};
 
 	function getNumber (field, options, ratio) {
@@ -34,49 +44,6 @@
 		var difference = end - start;
 		var change = difference * ratio;
 		return start + change;
-	}
-
-	function OptionsParallax (element, options) {
-		this.options = options;
-		if (!this.options.start) {
-			this.options.start = {};
-		}
-		if (!this.options.easing) {
-			this.options.easing = {};
-		}
-		this.element = element;
-	}
-
-	OptionsParallax.prototype.handleScroll = function (ratio) {
-		var transformString = 'translate3D(';
-		if (this.options.end.x) {
-			transformString += getNumber('x', this.options, ratio) + 'px,';
-		} else {
-			transformString += '0px,';
-		}
-		if (this.options.end.y) {
-			transformString += getNumber('y', this.options, ratio) + 'px,';
-		} else {
-			transformString += '0px,';
-		}
-		if (this.options.end.z) {
-			transformString += getNumber('z', this.options, ratio) + 'px)';
-		} else {
-			transformString += '0px)';
-		}
-
-		if (this.options.end.rotate) {
-			transformString += ' rotate(' + getNumber('rotate', this.options, ratio) + 'deg)';
-		}
-
-		if (this.options.end.scale) {
-			transformString += ' scale(' + getNumber('scale', this.options, ratio) + ')';
-		}
-		this.element.style.transform = transformString;
-
-		if (this.options.end.opacity) {
-			this.element.style.opacity = getNumber('opacity', this.options, ratio);
-		}
 	};
 
 	function Root (element, offsets) {
@@ -84,19 +51,16 @@
 		this.items = [];
 
 		this.pxThru = 0;
-		this.ratio = 0;
 
 		var self = this;
 
 		function handleScroll () {
+			
 			var start = Math.max(0, self.watcher.top - scrollMonitor.viewportHeight);
-			var end = Math.min(self.watcher.bottom, scrollMonitor.documentHeight - scrollMonitor.viewportHeight);
-
 			self.pxThru = Math.max(0, scrollMonitor.viewportTop - start);
-			self.ratio = self.pxThru / (end - start);
 
 			for (var i=0; i < self.items.length; i++) {
-				self.items[i].handleScroll.call(self.items[i], self.ratio, self.pxThru);
+				self.items[i].handleScroll.call(self.items[i], self.pxThru, self.watcher.height);
 			}
 		}
 
@@ -106,16 +70,10 @@
 		this.watcher.exitViewport(function () {
 			window.removeEventListener('scroll', handleScroll);
 		});
-	}
+	};
 
-	Root.prototype.add = function add (element, optionsOrSpeed) {
-		var newItem;
-		if (typeof optionsOrSpeed === 'number') {
-			newItem = new SpeedParallax(element, optionsOrSpeed);
-		} else {
-			newItem = new OptionsParallax(element, optionsOrSpeed);
-		}
-
+	Root.prototype.add = function add (element, speed, fade, easing) {
+		var newItem = new SpeedParallax(element, speed, fade, easing);
 		this.items.push(newItem);
 	};
 
